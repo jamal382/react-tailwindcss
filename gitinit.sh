70517a1f600b334b4f0203339c570e3a3c5d633b
jamal.khan <jamal.khan@brotecs.com>
2025-06-01 11:29:21 +0600

    Add script to initiate project

---
+++ gitinit.sh
@@ -0,0 +1,8 @@
+!/usr/bin/bash
+echo "# react-tailwindcss" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/jamal382/react-tailwindcss.git
+git push -u origin main
